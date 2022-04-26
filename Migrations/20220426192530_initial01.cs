9edb3aaafe0c3ffd46da6a28abc256e6e87651dd
santos <antonioars857@gmail.com>
2022-04-26 14:41:59 -0500

    first commit

---
+++ Migrations/20220426192530_initial01.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ControlIngresoGasto.Migrations
+{
+    public partial class initial01 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
